10eaf3b44038f6bebaf260f389ca1f229126efa5
Simon Wells <siwells@gmail.com>
2015-10-01 12:20:39 +0100

    Initial Selection Sort problem scaffolding code

---
+++ code/lab2/problems/SelectionSort.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class SelectionSort
+{
+ 
+    public static void selectionSort(int[] arr)
+    {
+         
+
+
+    }
+     
+    public static void main(String[] args)
+    {
+         
+        int[] data = {10,34,2,56,7,67,88,42};
+        System.out.println("Unsorted Array "+Arrays.toString(data));
+
+        selectionSort(data);
+        System.out.println("Sorted Array "+Arrays.toString(data));
+    }
+}
